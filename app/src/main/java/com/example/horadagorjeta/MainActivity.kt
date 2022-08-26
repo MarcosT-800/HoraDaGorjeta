24c459d0a1d78db2f8af849bbe605c1c5e23c226
Marcos Alves <82832426+MarcosT-800@users.noreply.github.com>
2022-08-26 18:46:11 -0300

    Main

--- app/src/main/java/com/example/horadagorjeta/MainActivity.kt
+++ app/src/main/java/com/example/horadagorjeta/MainActivity.kt
@@ -4,8 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener{ calculateTip() }
+   }
+
+   private fun calculateTip() {
+    val stringInTextField = binding.costOfService.text.toString()
+    val cost = stringInTextField.toDoubleOrNull()
+    if (cost == null) {
+        binding.tipResult.text = ""
+        return
     }
+    val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+    R.id.option_twenty_percent -> 0.20
+    R.id.option_eighteen_percent -> 0.18
+    else -> 0.15
+    }
+
+    var tip = tipPercentage * cost
+    is (binding.roundUpSwitch.isChecked) {
+       tip = kotlin.math.cell(tip)
+    }
+
+    val fromattedTip = NumberFormat.getCurrencyInstance().format(tip)
+    binding.tipResult.text = getString(R.string.tip_amount, formatted)
+ 
+   }
 }
